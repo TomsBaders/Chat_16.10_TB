eb75ac267706059a41e22accbe79406af5e8e83b
[Students] Toms Baders <93DPTBaders@rvt.lv>
2020-10-16 16:47:58 +0300

    Second Commit

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("home_page.html")
+
+@app.route("/chat")
+def chat():
     return render_template("chat.html")
+
+@app.route("/friends")
+def friends():
+    return render_template("friends.html")
 
 
 app.run(host="0.0.0.0", port=80, debug=True)
